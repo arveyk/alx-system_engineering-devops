11c082702aff2ec37227fa27fe8be4ea6c249bf9
Harvey W. Kis <harveykisiangani@gmail.com>
2023-10-07 00:13:46 +0300

    character count

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -3,7 +3,7 @@
 
 sender =  ARGV[0].scan(/from:(\+?.{11})/).join
 receiver = ARGV[0].scan(/to:(\+?\d{11,11})/).join
-flags = ARGV[0].scan(/-?\d:-?\d:-?\d:-?\d/).join
+flags = ARGV[0].scan(/flags:(-?\d:-?\d:-?\d:-?\d:-?\d)/).join
 
 res = sender + "," + receiver + "," + flags
 puts res
