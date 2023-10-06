1885a45bc918901359a622c90213f8834d975baf
Harvey W. Kis <harveykisiangani@gmail.com>
2023-10-06 23:59:38 +0300

    separate variable capture sender receiver and flags

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,12 @@
 #!/usr/bin/env ruby
 # Script for regex matching only capital letters
-puts ARGV[0].scan(/\+\d{11,11}(.*?)/).join
-puts ARGV[0].scan(/[01]/).join
+
+sender =  ARGV[0].scan(/from:(\+?.{11})/).join
+receiver = ARGV[0].scan(/to:(\+?\d{11,11})/).join
+flags = ARGV[0].scan(/-?\d:-?\d:-?\d:-?\d/).join
+
+res = sender + "," + receiver + "," + flags
+puts res
+# puts ARGV[0].scan(/from:([a-zA-Z]*)/)
+# puts ARGV[0].scan(/to:(\+?\d{11,11})/).join
+# puts ARGV[0].scan(/-?\d:-?\d:-?\d:[01]:-?\d/).join
