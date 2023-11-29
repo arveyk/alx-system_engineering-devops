e15ef7cd6b52520e29621c082bc38f447616ec38
Harvey W. Kis <harveykisiangani@gmail.com>
2023-11-30 01:14:09 +0300

    exec changed to package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,8 @@
 # Puppet Package installer
-exec { 'flask'
-    command  => 'apt-install flask'
+package { 'flask'
+#    command  => 'apt-install flask'   
     ensure   => '2.1.0'
     provider => 'pip3'
+#    install_options => ['']
 }
 
