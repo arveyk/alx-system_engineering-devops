e811a4d66074d9753b077f116ae91df316ab6b22
Harvey W. Kis <harveykisiangani@gmail.com>
2023-12-13 10:06:25 +0300

    flask version 2.1.0 installed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,11 @@
 # Puppet Package installer
-exec { 'pip3 install flask==2.1.0':
+$your_version = '2.1.0'
+package { 'pip install flask':
+    ensure   => '2.1.0',
+    provider => pip3,
+}
+exec { "pip3 install --force-reinstall 'flask==2.1.0'":
+    path     => '/usr/bin:/usr/sbin:/bin',
     provider => shell,
+    onlyif => "flask - | grep 'Version ${your_version}'"
 }
