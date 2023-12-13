5aeb2f64b8f07451e0df8a85d0c556db06e97f69
Harvey W. Kis <harveykisiangani@gmail.com>
2023-12-13 10:09:11 +0300

    exec command commented out

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,8 +4,8 @@ package { 'pip install flask':
     ensure   => '2.1.0',
     provider => pip3,
 }
-exec { "pip3 install --force-reinstall 'flask==2.1.0'":
-    path     => '/usr/bin:/usr/sbin:/bin',
-    provider => shell,
-    onlyif => "flask - | grep 'Version ${your_version}'"
-}
+#exec { "pip3 install --force-reinstall 'flask==2.1.0'":
+#    path     => '/usr/bin:/usr/sbin:/bin',
+#    provider => shell,
+#    onlyif   => "flask - | grep 'Version ${your_version}'"
+#}
