21121584a4965bec165720cf8c11c6ea382f747b
Harvey W. Kis <harveykisiangani@gmail.com>
2024-01-05 22:03:38 +0300

    prevent execution when imported

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -10,26 +10,26 @@ url = "https://jsonplaceholder.typicode.com/todos/" + employee_id
 response = requests.get(url)
 json_resp = response.json()
 
-print("{}".format(json_resp))
+if __name__ == '__main__':
+    # print("{}".format(json_resp))
+    names = ["EMPLOYEE_NAME",
+             "NUMBER_OF_TASKS",
+             "TOTAL_NUMBER_OF_TASKS"
+             ]
 
-names = ["EMPLOYEE_NAME",
-         "NUMBER_OF_TASKS",
-         "TOTAL_NUMBER_OF_TASKS"
-         ]
+    for name in json_resp:
+        if json_resp[name]:
+            print(json_resp[name])
 
-for name in json_resp:
-    if json_resp[name]:
-        print(json_resp[name])
+    print('Employee {} is done with'.format(json_resp['completed']))
+    print('\t{}'.format(json_resp['title']))
 
-print('Employee {} is done with'.format(json_resp['completed']))
-print('\t{}'.format(json_resp['title']))
+    with open("response.text", "w") as file_json:
+        file_json.write(response.text)
 
-with open("response.text", "w") as file_json:
-    file_json.write(response.text)
+    with open("USER.csv", "w") as user_csv:
+        fields = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+        csv_write = csv.DictWriter(user_csv, fieldnames=fields)
 
-with open("USER.csv", "w") as user_csv:
-    fields = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-    csv_write = csv.DictWriter(user_csv, fieldnames=fields)
-
-    for line in response.text:
-        csv_write.writerow(line)
+        for line in response.text:
+            csv_write.writerow(line)
