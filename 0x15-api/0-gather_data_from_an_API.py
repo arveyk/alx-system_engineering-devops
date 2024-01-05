25ef4927523bcfb5123d9fc8b8dbf6e884b76ced
Harvey W. Kis <harveykisiangani@gmail.com>
2024-01-05 21:57:58 +0300

    pydoc!

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,30 +1,8 @@
 #!/usr/bin/python3
-""" Script that returns an employees info using RESTful api
-"""
+""" Script that returns an employees info using RESTful api """
 import requests
 import sys
 
-employee_id = sys.argv[1]
-url = "https://jsonplaceholder.typicode.com/todos/" + employee_id
-response = requests.get(url)
-json_resp = response.json()
-
-print("{}".format(json_resp))
-
-names = ["EMPLOYEE_NAME",
-         "NUMBER_OF_TASKS",
-         "TOTAL_NUMBER_OF_TASKS"
-         ]
-
-for name in json_resp:
-    if json_resp[name]:
-        print(json_resp[name])
-
-print('Employee {} is done with'.format(json_resp['completed']))
-print('\t{}'.format(json_resp['title']))
-
-with open("response.text", "w") as file_json:
-    file_json.write(response.text)
 
 # print ("Employee EMPLOYEE_NAME is done with tasks
 # (NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
@@ -33,3 +11,26 @@ with open("response.text", "w") as file_json:
 #        TOTAL_NUMBER_OF_TASKS: total number
 # f tasks, which is the sum of completed and non-completed tasks
 #                "
+
+if __name__ == '__main__':
+    """ This ensures the module is not executes when imported """
+    employee_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/todos/" + employee_id
+    response = requests.get(url)
+    json_resp = response.json()
+
+    # print("{}".format(json_resp))
+
+    names = ["EMPLOYEE_NAME",
+             "NUMBER_OF_TASKS",
+             "TOTAL_NUMBER_OF_TASKS"
+             ]
+    for name in json_resp:
+        if json_resp[name]:
+            print(json_resp[name])
+
+    print('Employee {} is done with'.format(json_resp['completed']))
+    print('\t{}'.format(json_resp['title']))
+
+    with open("response.text", "w") as file_json:
+        file_json.write(response.text)
