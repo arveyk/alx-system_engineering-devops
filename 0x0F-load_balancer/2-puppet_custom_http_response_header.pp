77ba7c45a4aa7398be8efff438d86e7becc7f418
Harvey W. Kis <harveykisiangani@gmail.com>
2024-02-28 11:23:28 +0300

    custom http response by puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,10 @@
+# puppet manifest to install and config nginx
+# class {"nginx"}
+class:
+ -nginx:: snippet
+ nginx:: snippet:
+conf: | 
+	location = nginx_404 {
+	  internal;
+	  add_header Content-Type text/javascript;
+	}
